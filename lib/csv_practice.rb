1a880e2f19e705a18dc796ec3e8f4487c6de0f56
CheezItMan <chris@adadevelopersacademy.org>
2018-08-10 14:46:33 -0700

    solution complete

--- lib/csv_practice.rb
+++ lib/csv_practice.rb
@@ -14,27 +14,54 @@ def load_data(filename)
 end
 
 def total_medals_per_country(olympic_data)
+  total_medals = {}
+  olympic_data.each do |athlete|
+    unless athlete['Medal'] == 'NA'
+      if total_medals[athlete['Team']]
+        total_medals[athlete['Team']] += 1
+      else
+        total_medals[athlete['Team']] = 1
+      end
+    end
+  end
 
+    return total_medals.map do |country, medals|
+      {country: country, total_medals: medals}
+    end
 end
 
 def save_medal_totals(filename, medal_totals)
-
+  CSV.open(filename, 'w', :write_headers=> true, headers: medal_totals.first.keys) do |csv|
+    medal_totals.each do |country_entry|
+      csv << country_entry
+    end
+  end
 end
 
 # Part 2 - More Enumerable Practice
 
 def all_gold_medal_winners(olympic_data)
-
+  olympic_data.select do |entry|
+    entry['Medal'] == 'Gold'
+  end
 end
 
-def metals_sorted_by_country(metal_totals)
-
+def medals_sorted_by_country(medal_totals)
+  medal_totals.sort_by do |entry|
+    entry[:country]
+  end
 end
 
-def country_with_most_metals(metal_totals)
-
+def country_with_most_medals(medal_totals)
+  medal_totals.max_by do |entry|
+    entry[:total_medals]
+  end
 end
 
 def athlete_height_in_inches(olympic_data)
-
+  olympic_data.map do |entry|
+    entry = entry.clone
+    entry['Height'] = entry['Height'].to_f / 2.5
+    entry
+  end
 end
--- specs/csv_practice_spec.rb
+++ specs/csv_practice_spec.rb
@@ -10,19 +10,22 @@ require_relative '../lib/csv_practice'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
+CSV_HEADERS = %w[ID Name Sex Age Height Weight Team NOC Games Year Season City Sport Event Medal]
+MEDAL_TOTALS_FILENAME = 'data/medal_totals.csv'
+OLYMPIC_DATA_FILENAME = 'data/athlete_events.csv'
+
 describe 'CSV Practice Methods' do
   describe 'load_data' do
     it 'returns an array of hashes' do
       # Arrange & Act
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
 
       # Assert
       expect(data.class).must_equal Array
       data.each do |data_row|
         expect(data_row.class).must_equal Hash
-        expected_keys = %w[cdatetime address district beat grid crimedescr latitude longitude]
 
-        expected_keys.each do |key|
+        CSV_HEADERS.each do |key|
           expect(data_row.key?(key)).must_equal true
         end
       end
@@ -30,92 +33,81 @@ describe 'CSV Practice Methods' do
 
     it 'has the proper number of rows' do
       # Arrange & Act
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
 
       # Assert
-      expect(data.length).must_equal 7584
+      expect(data.length).must_equal 49503
     end
 
-    it 'has the right 1st row' do
+    it 'has the right 1st and last row' do
       # Arrange & Act
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
 
       # Assert
-      expect(data.first['cdatetime']).must_equal '1/18/06 4:40'
-      expect(data.first['address']).must_equal 'SAN JUAN RD / EL CENTRO RD'
-      expect(data.first['district']).must_equal '1'
-    end
-
-    it 'has the right last row' do
-      # Arrange & Act
-      data = load_data('data/athlete_events.csv')
-
-      # Assert
-      expect(data.last['cdatetime']).must_equal '1/9/06 9:30'
-      expect(data.last['address']).must_equal '14TH AVE / POWER INN RD'
-      expect(data.last['district']).must_equal '6'
+      expect(data.first['ID']).must_equal '21'
+      expect(data.first['Name']).must_equal 'Ragnhild Margrethe Aamodt'
+      expect(data.first['Team']).must_equal 'Norway'
+      expect(data.last['ID']).must_equal '135568'
+      expect(data.last['Name']).must_equal 'Olga Igorevna Zyuzkova'
+      expect(data.last['Team']).must_equal 'Belarus'
     end
   end
 
-  describe 'total_crimes_per_district' do
+  describe 'total_medals_per_country' do
+    # Arrange
+    data = load_data(OLYMPIC_DATA_FILENAME)
+
     correct_totals = {
-      '1' => 868,
-      '2' => 1462,
-      '3' => 1575,
-      '4' => 1161,
-      '5' => 1159,
-      '6' => 1359
+      'Norway' => 133,
+      'United States' => 944,
+      'Canada' => 321,
+      'Russia' => 470,
+      'China' => 423,
+      'Bahrain' => 3,
+      'Jamaica' => 69,
+      'United Arab Emirates' => 1
     }
 
-    it 'should return a hash' do
-      # Arrange
-      data = load_data('data/athlete_events.csv')
+    it 'should return an array of hashes' do
 
       # Act
-      total_crimes = total_crimes_per_district(data)
+      total_medals = total_medals_per_country(data)
 
       # Assert
-      expect(total_crimes).must_be_instance_of Hash
-    end
+      expect(total_medals).must_be_instance_of Array
+      correct_totals.keys.each do |country|
+        country_total = total_medals.find do |medal_total|
+          medal_total[:country] == country
+        end
 
-    it 'should calculate the proper totals' do
-      # Arrange
-      data = load_data('data/athlete_events.csv')
-
-      # Act
-      total_crimes = total_crimes_per_district(data)
-
-      # Assert
-
-      correct_totals.keys.each do |key|
-        expect(total_crimes[key]).must_equal correct_totals[key]
+        expect(country_total[:total_medals]).must_equal correct_totals[country]
       end
     end
   end
 
-  describe 'Can create a total crimes file' do
+  describe 'Can create a total medal file' do
     it 'The file is created' do
       # Arrange
-      data = load_data('data/athlete_events.csv')
-      total_crimes = total_crimes_per_district(data)
+      data = load_data(OLYMPIC_DATA_FILENAME)
+      medal_totals = total_medals_per_country(data)
 
       # Act
-      save_crime_totals('data/total_crimes.csv', total_crimes)
+      save_medal_totals(MEDAL_TOTALS_FILENAME, medal_totals)
 
       # Assert
-      expect(File.exist?('data/total_crimes.csv')).must_equal true
+      expect(File.exist?(MEDAL_TOTALS_FILENAME)).must_equal true
     end
 
     it 'The file contains the right entries and headers' do
       # Arrange
-      data = load_data('data/athlete_events.csv')
-      total_crimes = total_crimes_per_district(data)
+      data = load_data(OLYMPIC_DATA_FILENAME)
+      medal_totals = total_medals_per_country(data)
 
       # Act
-      save_crime_totals('data/total_crimes.csv', total_crimes)
+      save_medal_totals(MEDAL_TOTALS_FILENAME, medal_totals)
 
       # Assert
-      CSV.read('data/total_crimes.csv', headers: true) do |line|
+      CSV.read(OLYMPIC_DATA_FILENAME, headers: true) do |line|
         correct_totals.keys.each do |key|
           expect(line[key]).must_equal correct_totals[key]
         end
@@ -123,93 +115,86 @@ describe 'CSV Practice Methods' do
     end
   end
 
-  describe 'all_burglary_crimes' do
+  describe 'All Gold Medal Winners' do
+    # Arrange
+    data = load_data(OLYMPIC_DATA_FILENAME)
+
+    # Act
+    gold_medal_winners = all_gold_medal_winners(data)
+
     it 'returns an array of hashes' do
-      # Arrange
-      data = load_data('data/athlete_events.csv')
-
-      # Act
-      burglaries = all_burglary_crimes(data)
 
       # Assert
-      expect(burglaries.class).must_equal Array
-      burglaries.each do |crime|
-        expect(crime.class).must_equal Hash
+      expect(gold_medal_winners.class).must_equal Array
+      gold_medal_winners.each do |winner|
+        expect(winner.class).must_equal Hash
       end
     end
 
-    it 'contains only crimes with the word `burglary` in the description' do
+    it 'They all have `Gold` in the medal field' do
+      # Assert
+      gold_medal_winners.each do |winner|
+        expect(winner['Medal'].upcase.include? 'GOLD').must_equal true
+      end
+    end
+
+    it 'has the correct number of gold medalists' do
+      count = data.sum do |athlete|
+        athlete['Medal'].upcase == 'GOLD' ? 1 : 0
+      end
+      expect(gold_medal_winners.length).must_equal count
+    end
+  end
+
+  describe 'country_totals_sorted_by_country' do
+    it 'orders all the total medal reports' do
       # Arrange
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
+      medal_totals = total_medals_per_country(data)
 
       # Act
-      burglaries = all_burglary_crimes(data)
+       sorted_medal_list = medals_sorted_by_country(medal_totals)
 
       # Assert
-      burglaries.each do |crime|
-        expect(crime['crimedescr'].upcase.include? 'BURGLARY').must_equal true
+      last_medal = sorted_medal_list.first
+      sorted_medal_list.each do |current_entry|
+        expect(last_medal[:country] <= current_entry[:country]).must_equal true
+
+        last_medal = current_entry
       end
     end
   end
 
-  describe 'crimes_sorted_by_date' do
-    it 'orders all the crime reports' do
-      # Arrange
-      data = load_data('data/athlete_events.csv')
-
-      # Act
-      ordered_crimes = crimes_sorted_by_date(data)
-
-      # Assert
-      last_crime = DateTime.strptime('1/1/00', '%m/%d/%Y')
-      ordered_crimes.each do |crime|
-        crime_date = DateTime.strptime(crime['cdatetime'], '%m/%d/%Y %H:%M')
-        expect(crime_date >= last_crime).must_equal true
-
-        last_crime = crime_date
-      end
-    end
-  end
-
-  describe 'district_with_most_crime' do
+  describe 'total_medals_per_country' do
     it 'returns the right value' do
       # Arrange
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
+      medal_totals = total_medals_per_country(data)
+
 
       # Act
-      high_crime_district = district_with_most_crime(data)
+      most_medals = country_with_most_medals(medal_totals)
 
       # Assert
-      expect(high_crime_district).must_equal '3'
+      expect(most_medals[:country]).must_equal 'United States'
     end
   end
 
-  describe 'district_with_least_crime' do
-    it 'returns the right value' do
+  describe 'athlete_height_in_inches' do
+    it 'correctly converts the height to inches' do
       # Arrange
-      data = load_data('data/athlete_events.csv')
+      data = load_data(OLYMPIC_DATA_FILENAME)
 
       # Act
-      high_crime_district = district_with_least_crime(data)
+      converted_data = athlete_height_in_inches(data)
 
       # Assert
-      expect(high_crime_district).must_equal '1'
-    end
-  end
-
-  describe 'crime_coordinates' do
-    it 'returns an array of 2-element arrays' do
-      # Arrange
-      data = load_data('data/athlete_events.csv')
-
-      # Act
-      coordinates = crime_coordinates(data)
-
-      # Assert
-      expect(coordinates.class).must_equal Array
-      coordinates.each do |coordinate|
-        expect(coordinate.class).must_equal Array
-        expect(coordinate.length).must_equal 2
+      expect(converted_data.class).must_equal Array
+      converted_data.each_with_index do |entry, index|
+        expect(entry.class).must_equal Hash
+        # puts "#{data[index]}"
+        # puts "Converted data #{entry}"
+        expect(entry['Height']).must_be_close_to(data[index]['Height'].to_f / 2.5)
       end
     end
   end
