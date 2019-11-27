0f9188142cf6909002bfe20515d78792f3e4a62f
beccaelenzil <beccaelenzil@gmail.com>
2019-11-27 10:43:31 -0800

    up to date

--- csv-pratice-brut-test.rb
+++ csv-pratice-brut-test.rb
@@ -4,6 +4,6 @@ require_relative "lib/csv_practice.rb"
 filename = "data/athlete_events.csv"
 olympic_data = load_data(filename)
 medal_totals = total_medals_per_country(olympic_data)
-#puts medal_totals.first.class
+puts medal_totals
 save_medal_totals("medal_totals.csv", medal_totals)
-#ap athlete_height_in_inches(olympic_data)
+ap athlete_height_in_inches(olympic_data)
